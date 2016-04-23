4b630640e6fc5bed85d08c626d52140a26d3ae76
Howard Xiao <dargasia@users.noreply.github.com>
2016-04-22 17:29:06 -0700

    Add a time towards the end. Before I get a supervisor set up, at least this confuses people less.

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ try:
         messages.append(bart.get_predictions())
         messages.append(muni.get_predictions())
         text = '<FI>'.join(messages)
-        display_text = "<ID01><PA>  <FD>{}  \r\n".format(text)
+        display_text = "<ID01><PA>  <FD>{}{}  \r\n".format(text, datetime.now().strftime('%H:%M'))
         print display_text
         subprocess.call('printf "{text}" > /dev/ttyS0'.format(text=display_text), shell=True)
         sleep(60)
