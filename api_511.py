a9f3faf98524854d9879fdb18922311a6a5c0aff
Roger Filmyer <roger.filmyer@gmail.com>
2016-07-10 17:25:08 -0700

    Refactored some BART code and added a bunch of docstrings

--- api_511.py
+++ api_511.py
@@ -9,23 +9,28 @@ import requests
 REQUEST_PARAMS = {'stopcode': '15553', 'token': 'ebda4c89-0c5f-40d8-9ed8-e9deff999a49'}
 API_URL = 'http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx'
 
+
 class TransitServiceError(Exception):
     """
     An error from the request to the 511.org API.
     """
     pass
 
+
 def request_511_xml(stopcode='15553'):
-    # type: (str) -> dict(str: list(str))
     """
     Sends a request to the 511.org XML API with a station code and 
     returns a dict of lines that serve it and the next predicted arrival times.
 
+    Args:
+        stopcode: The code uniquely identifying a MUNI stop, usually visible on a nearby sign post.
     Returns:
-        OrderedDict: Lines and ETAs for routes at the station in the format
-            OrderedDict([('J', ['2', '11']), ('KT', ['5', '16']), ('L', ['4', '6']),
-             ('M', ['1', '9']), ('N', ['4', '10'])])
+        OrderedDict: Lines and ETAs for routes at the station. Exists as an OrderedDict for predictibility's sake.
 
+    Examples:
+        >>>request_511_xml('15731') # Montgomery Station Inbound
+        OrderedDict([('J', ['2', '11']), ('KT', ['5', '16']), ('L', ['4', '6']),
+             ('M', ['1', '9']), ('N', ['4', '10'])])
     """
     api_request = REQUEST_PARAMS
     api_request['stopcode'] = stopcode
@@ -33,7 +38,7 @@ def request_511_xml(stopcode='15553'):
     root = ElementTree.fromstring(xml_string)
     if root.tag == 'transitServiceError':
         raise TransitServiceError(root.text)
-    bus_lines = root.findall(".//Route") # If only they used jQuery-style selectors
+    bus_lines = root.findall(".//Route")  # If only they used jQuery-style selectors
     predictions = OrderedDict()
     for bus in bus_lines:
         arrival_times = []
@@ -43,14 +48,45 @@ def request_511_xml(stopcode='15553'):
         predictions[bus.attrib.get('Code')] = arrival_times
     return predictions
 
-def format_route_times(route, bus_times, direction = ''):
-    # type: (str, str) -> str
+
+def format_route_times(route, bus_times, direction=''):
+    """
+    Formats prediction times for an individual bus line at a given stop.
+
+    Args:
+        route (str): A bus route (ex: 33, 14R, J) that has predicted ETAs
+        bus_times (list): Next arrival times for a given bus route (ex: [2, 22, 42])
+        direction (str, optional): A optional direction to be sent along with the bus parameters
+
+    Returns:
+        str: The final, formatted string with routes and times
+
+    Examples:
+        >>> format_route_times('14', [3, 11, 17], 'NB')
+        "<CM>14 <CF>NB <CB>3, 11, 17"
+    """
     route_info = "<CM>{route} <CF>{dir} <CB>{times}"
     minutes = ','.join([x for x in bus_times if int(x) <= 120])  # Get rid of abnormal predictions.
     return route_info.format(route=route, times=minutes, dir=direction)
 
+
 def format_service_prediction(headline, station_codes):
-    # type: (str, dict) -> str
+    """
+    Returns a formatted string combining prediction times for different stops and lines
+        under a single banner for a provider.
+
+    Args:
+        headline (str): Stop predictions will be preceded with this header,
+            useful for distinguishing different transit providers.
+        station_codes(dict): A dictionary where keys map to stop codes, and
+
+    Returns:
+        string: A formatted string for
+
+    Examples:
+        >>>format_service_prediction("MUNI Arrivals", {'15553': 'NB', '13338': 'WB'})
+        "<CP>MUNI Arrivals<FI><CM>14 <CF>NB <CB>3, 11, 17<CM>33 <CF>WB <CB>22, 40"
+    """
     opening = "<CP>{headline}<FI>".format(headline=headline)
     direction_color = "<SA>{routes}"
     service_predictions = []
@@ -59,7 +95,7 @@ def format_service_prediction(headline, station_codes):
         predictions = request_511_xml(station_code)
         for route, times in predictions.items():
             if times:
-                #print('route:', route, 'times:', times, 'direction:', direction)
+                # print('route:', route, 'times:', times, 'direction:', direction)
                 route_predictions.append(format_route_times(route, times, direction))
         formatted_route_predictions = '<FI>'.join(route_predictions)
         service_predictions.append(
@@ -67,9 +103,16 @@ def format_service_prediction(headline, station_codes):
 
     return opening + "<FI>".join(service_predictions)
 
+
 def predict():
-    # type: () -> list(str)
-    muni = {"name": "MUNI Arrivals", 
+    """
+    Pulls bus prediction info for some preconfigured stops, routes, and providers.
+        This function exists for convenience, and is just sugar over the format_service_prediction function.
+
+    Returns:
+        list: String(s) containing formatted route information for given providers and service times.
+    """
+    muni = {"name": "MUNI Arrivals",
             "stops": OrderedDict([('15553', 'NB'), ('13338', 'WB'), ('15554', 'SB')])}
     caltrain = {"name": "Caltrain@22nd", "stops": OrderedDict([('70022', 'SB')])}
     services = [muni, caltrain]
--- bart.py
+++ bart.py
@@ -1,14 +1,22 @@
 import requests
-import json
 from xml.etree import ElementTree
-from collections import defaultdict
 
-REQUEST_PARAMS = {'cmd': 'etd', 'orig': '16TH', 'key': 'MW9S-E7SL-26DU-VV8V'}
+API_KEY = 'MW9S-E7SL-26DU-VV8V'
 BART_URL = 'http://api.bart.gov/api/etd.aspx'
 
 
 def get_bart_times(params=REQUEST_PARAMS):
-    # type: (dict) -> dict
+    """
+    Makes a request to BART's XML API
+
+    Args:
+        params (dict): Parameters that will be passed to the HTTP request
+            in the style {"key": "value"} -> "index.html?key=value"
+
+    Returns:
+        list: A list of dicts with the keys "line", "color", and "times"
+            corresponding to prediction times on each line for a given station.
+    """
     xml_string = requests.get(BART_URL, params=params).text
     root = ElementTree.fromstring(xml_string)
     
@@ -23,23 +31,33 @@ def get_bart_times(params=REQUEST_PARAMS):
     return predictions
 
 
+def format_bart_information(destination, etas):
+    """
+    Combines a route name and a list of times into a string.
 
-def format_bart_information(destination, etas, color=None):
-    # type: (str, list(int)) -> str
+    Args:
+        destination (str): Where a train is headed.
+        etas (list): A list of arrival times for a train line (can be `str`s or `int`s)
+
+    Returns:
+        string: The formatted string, ready to be put into the board.
+    """
     prediction = '<SA><CM>{}<CB> {}'.format(destination, ','.join(etas))
     return prediction
 
 def get_predictions(orig='16TH'):
     # type: (str) -> str
-    api_params = REQUEST_PARAMS
-    api_params['orig'] = orig
+    """
+    Given a BART stop, returns predictions formatted for the LED board.
+    """
+    api_params = {"cmd": "etd", "orig": orig, "key": API_KEY}
 
     bart_text = '<CP>BART Arrivals<FI>'
     arrivals = get_bart_times(api_params)
     line_predictions = []
 
     for line in arrivals:
-        line_predictions.append(format_bart_information(line['line'], line['times'], line['color']))
+        line_predictions.append(format_bart_information(line['line'], line['times']))
 
     bart_text += '<FI>'.join(line_predictions)
 
