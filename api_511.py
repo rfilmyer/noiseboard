e0d6590137bab61f42c0379992315220ea8246d9
Roger Filmyer <roger.filmyer@gmail.com>
2016-07-12 21:24:57 -0700

    spacing

--- api_511.py
+++ api_511.py
@@ -122,13 +122,14 @@ def format_route_times(route, bus_times, direction='', mapping=None):
     if mapping:
         if mapping.get(route):
             route = mapping.get(route)
-    route_info_fmt = "<CM>{route} <CF>{dir} <CB>{times}"
-    route_info_text = "{route} ({dir}): {times}"
+    route_info_fmt = "<CM>{route}{dir} <CB>{times}"
+    route_info_text = "{route}{dir}: {times}"
 
     minutes = ','.join([str(x) for x in bus_times if int(x) <= 120])  # Get rid of abnormal predictions.
-
-    return {"fmt":  route_info_fmt.format( route=route, times=minutes, dir=direction),
-            "text": route_info_text.format(route=route, times=minutes, dir=direction)}
+    direction_fmt = ' <CF>' + direction + ' ' if direction else ''
+    direction_text = ' (' + direction + ')' if direction else ''
+    return {"fmt":  route_info_fmt.format( route=route, times=minutes, dir=direction_fmt),
+            "text": route_info_text.format(route=route, times=minutes, dir=direction_text)}
 
 
 def format_service_prediction(headline, agency, station_codes, mapping=None, legacy=False):
