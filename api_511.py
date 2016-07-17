b78158bf49d2ae9886361fed4fd91ec0f4bbcfcd
Roger Filmyer <roger.filmyer@gmail.com>
2016-07-17 02:11:59 +0000

    Added missing BART line, only 2 predictions

--- api_511.py
+++ api_511.py
@@ -292,7 +292,7 @@ class TransitPredictor(object):
                 etas = []
                 for time in times:
                     eta = get_minutes_until_arrival(time)
-                    if eta >= 0 and len(etas) <= 2:
+                    if eta >= 0 and len(etas) < 2:
                         etas.append(eta)
                 stop_etas[line] = etas
             prediction_etas[station_code] = stop_etas
--- default_transit_services.py
+++ default_transit_services.py
@@ -5,5 +5,6 @@ muni = {"name": "MUNI Arrivals", "agency": "sf-muni",
 caltrain = {"name": "Caltrain@22nd", "agency": "caltrain", "stops": OrderedDict([('70022', 'SB')])}
 bart = {"name": "BART", "agency": "bart", "stops": OrderedDict([('10', ''), ('99', '')]),
         "mapping": {"1561": "SFO/M", "385": "Daly", "389": "Daly", "720": "SFO",
-                    "1230": "Pitt", "237": "Rich", "736": "Frmt", "920": "Dubl",
-                    "764": "Mbrae", "243": "Daly", "722": "SFO", "917": "Frmt", "1351": "Rich"}}
+                    "1230": "Pitt", "736": "Frmt", "920": "Dubl",
+                    "764": "Mbrae", "243": "Daly", "722": "SFO", "917": "Frmt", 
+                    "1351": "Rich", "1009": "Rich", "237": "Rich"}}
